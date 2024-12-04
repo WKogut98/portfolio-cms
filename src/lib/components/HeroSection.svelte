0d405fadf0f90e3d51e0c2793e9a652e97261247
WKogut98 <wojciech.kogut7@gmail.com>
2024-12-04 17:45:05 +0100

    Hero Section

---
+++ src/lib/components/HeroSection.svelte
@@ -0,0 +1,13 @@
+<section class="hero-section default-margin">
+    <h3>Hello, I'm Nik</h3>
+    <div class = "underscore mb-m"></div>
+    <h1>I'm a Svelte developer crafting intuitive and performant</h1>
+    <h1 class="dark-grey">web applications</h1>
+    <button>Let's talk!</button>
+</section>
+
+<style>
+    .hero-section{
+        padding-top: 60px;
+    }
+</style>
