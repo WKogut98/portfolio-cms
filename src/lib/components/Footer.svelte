bf3c3dc80ad677a40a9735d800948d0a7e0283dc
WKogut98 <wojciech.kogut7@gmail.com>
2025-01-18 01:50:58 +0100

    commits desynched on deployment , makinhg silly change to see if it does something

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,6 +1,6 @@
 <footer class="footer">
     <div class="footer-container">
-        <h3 class="semi-bold mb-xs">Wojciech Kogut</h3>
+        <h3 class="semi-bold mb-xs">Wojciech Kogut AAA</h3>
         <div class ="mb-l">
             <a href="https://github.com/WKogut98" target="_blank" class="footer-link">GitHub</a>
             <a href="https://www.linkedin.com/in/wojciech-kogut-90b1b91b4/" target="_blank" class="footer-link">LinkedIn</a>
